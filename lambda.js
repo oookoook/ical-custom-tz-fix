defc0239633f40737c9d373823c72eedda9440ef
Adam Kučera <adam@nastojte.cz>
2025-09-09 21:13:02 +0200

    async hadnler fix

--- lambda.js
+++ lambda.js
@@ -1,3 +1,6 @@
 const serverlessExpress = require('@codegenie/serverless-express')
 const app = require('./api')
-exports.handler = async () => serverlessExpress({ app })
+exports.handler = async (event, context) => {
+    let serverlessExpressInstance = serverlessExpress({ app });
+    return serverlessExpressInstance(event, context);
+}
